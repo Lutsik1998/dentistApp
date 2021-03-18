48f84918fa5484f4d466626347472f28a7815d5f
vlutsenko245817 <245817@student.pwr.edu.pl>
2021-03-18 18:36:53 +0200

    init be

---
+++ src/main/java/com/dentistapp/dentistappdevelop/DentistAppDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.dentistapp.dentistappdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentistAppDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentistAppDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dentistapp/dentistappdevelop/DentistAppDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentistapp.dentistappdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentistAppDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
