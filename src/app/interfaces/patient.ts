aaae3e1185b34e535626cd9e817ac792508f4e9f
VladyslavBobrov <vbobrov1234@gmail.com>
2021-04-10 13:40:00 +0200

    add services and interfaces

---
+++ src/app/interfaces/patient.ts
@@ -0,0 +1,14 @@
+export interface Patient {
+    id: string;
+    email: string;
+    password: string;
+    firstName: string;
+    secondName: string;
+    lastName: string;
+    pesel: number;
+    birthDate : Date;
+    sex: string;
+    addres: string;
+    phoneNumber: number;
+    cardNumber: number;
+}
